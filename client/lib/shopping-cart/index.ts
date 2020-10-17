9336b1c3aa514160235d3ad851c08efdb854fea1
Payton Swick <payton@foolord.com>
2020-10-17 19:13:36 -0400

    Fix re-exported defaults in lib/shopping-cart

--- client/lib/shopping-cart/index.ts
+++ client/lib/shopping-cart/index.ts
@@ -1,4 +1,5 @@
-export * from './use-shopping-cart';
-export * from './shopping-cart-provider';
+export { default as useShoppingCart } from './use-shopping-cart';
+export { default as withShoppingCart } from './with-shopping-cart';
+export { default as ShoppingCartProvider } from './shopping-cart-provider';
 export * from './types';
 export * from './cart-functions';
